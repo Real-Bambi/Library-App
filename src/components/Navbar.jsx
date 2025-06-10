f0a6415206505b7599b499fde88fd07648c11058
Genevieve <genevieveappia2@gmail.com>
2025-06-10 10:49:25 +0000

    lib

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,42 @@
+import { } from 'lucide-react';
+
 export default function Navbar() {
     return (
         <nav className="flex justify-between">
-          
-    <div>
-      <button class="flex items-center gap-1 bg-gray-100 px-3 py-2 rounded-full text-sm">
-        All <i class="fa fa-chevron-down text-xs"></i>
-      </button>
-    </div>
 
-   
-    <div class="flex items-center bg-gray-100 px-3 py-2 rounded-full flex-grow max-w-md">
-      <i class="fa fa-search text-gray-500"></i>
-      <input type="text" placeholder="Search" class="bg-transparent outline-none px-2 w-full text-sm" />
-      <i class="fa fa-sliders text-gray-500 ml-2"></i>
-    </div>
 
-  
-    <div>
-      <button class="flex items-center gap-1 bg-gray-100 px-3 py-2 rounded-full text-sm">
-        <i class="fa fa-language text-red-400"></i> Lang <i class="fa fa-chevron-down text-xs"></i>
-      </button>
-    </div>
 
-   
-    <div class="flex items-center gap-2 bg-gray-100 px-3 py-2 rounded-full text-sm">
-      <i class="fa fa-clock text-red-400"></i>
-      <span>09:00 AM</span>
-    </div>
+            <select name="type" id="type" className=" flex items-center border-none shadow-lg bg-gray-100 rounded-full">
+                <option selected disabled>Categories</option>
+                <option value="drama">Fiction</option>
+                <option value="drama">Horror</option>
+                <option value="drama">Non Fiction</option>
+                <option value="drama">Drama</option>
 
-    
-    <div class="bg-gray-100 px-3 py-2 rounded-full text-sm">
-      4-Mar-2023
-    </div>
+            </select>
+            <div><input type="text" placeholder="Search" class="bg-white outline-none px-2 w-full text-sm" /></div>
+            <i class="fa fa-sliders text-gray-500 ml-2"></i>
 
-    
-    <div>
-      <button class="flex items-center gap-2 bg-gray-100 px-3 py-2 rounded-full text-sm">
-        <img src="https://i.pravatar.cc/30?img=12" alt="User" class="h-6 w-6 rounded-full" />
-        Kenson <i class="fa fa-chevron-down text-xs"></i>
-      </button>
-    </div>
+
+
+
+            <div class="flex items-center gap-2 bg-gray-100 px-3 py-2 rounded-full text-sm">
+                <i class="fa fa-clock text-red-400"></i>
+                <input type="time" placeholder='09:00 AM' />
+            </div>
+
+
+            <div class="bg-gray-100 px-3 py-2 rounded-full text-sm">
+                <input type="date" name="" id="" />
+            </div>
+
+
+            <div>
+                <button class="flex items-center gap-2 bg-gray-100 px-3 py-2 rounded-full text-sm">
+                    <img src="https://i.pravatar.cc/30?img=12" alt="User" class="h-6 w-6 rounded-full" />
+                    Kenson <i class="fa fa-chevron-down text-xs"></i>
+                </button>
+            </div>
         </nav>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,27 +4,27 @@ export default function Sidebar() {
 return (
     
  
-  <aside class="w-64 bg-white text-black flex flex-col justify-between py-8 px-6 rounded-tr-2xl rounded-br-2xl shadow-lg">
+  <aside className="w-64 bg-white text-black flex flex-col justify-between py-8 px-6 rounded-tr-2xl rounded-br-2xl shadow-xl shadow-blue-300">
     <div>
-      <div class="mb-10 text-left">
-        <h1 class="text-xl leading-none italic tracking-wide">
-          My <span class="text-[#F86F55] text-xl font-medium">Book</span><br />
-          <span class="tracking-widest text-3xl">Shelf</span>
+      <div className="mb-10 text-left">
+        <h1 className="text-xl leading-none italic tracking-wide">
+          My <span className="text-[#F86F55] text-xl font-medium">Book</span><br />
+          <span className="tracking-widest text-3xl">Shelf</span>
         </h1>
       </div>
 
       
-      <nav class="space-y-6 text-sm text-gray-700">
-        <a href="#" class="flex items-center gap-3 hover:text-orange-500">
+      <nav className="space-y-6 text-sm font-bold text-gray-700">
+        <a href="#" className="flex items-center gap-3 hover:text-orange-500">
          <Home /> Home
         </a>
-        <a href="#" class="flex items-center gap-3 hover:text-orange-500">
+        <a href="#" className="flex items-center gap-3 hover:text-orange-500">
           <Search /> Search
         </a>
-        <a href="#" class="flex items-center gap-3 hover:text-orange-500">
+        <a href="#" className="flex items-center gap-3 hover:text-orange-500">
            <LibraryBig /> My Shelf
         </a>
-        <a href="#" class="flex items-center gap-3 hover:text-orange-500">
+        <a href="#" className="flex items-center gap-3 hover:text-orange-500">
            <HandHeart /> Contribute
         </a>
       </nav>
@@ -32,9 +32,9 @@ return (
 
     
     <div class="space-y-2 text-xs text-gray-600 py-60">
-      <a href="#" class="hover:text-orange-500 block">About</a>
-      <a href="#" class="hover:text-orange-500 block">Support</a>
-      <a href="#" class="hover:text-orange-500 block">Terms & Condition</a>
+      <a href="#" className="hover:text-orange-500 block">About</a>
+      <a href="#" className="hover:text-orange-500 block">Support</a>
+      <a href="#" className="hover:text-orange-500 block">Terms & Condition</a>
     </div>
 
   </aside>
