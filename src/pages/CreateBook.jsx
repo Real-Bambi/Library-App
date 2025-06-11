3aebf7d4a38475c1fbfebc5e733b90d3b3e81a66
Dede <dedenyengor09@gmail.com>
2025-06-11 14:21:46 +0000

    Integration first commit

--- src/pages/CreateBook.jsx
+++ src/pages/CreateBook.jsx
@@ -1,65 +1,97 @@
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import { apiClient } from "../api/client";
 
 export default function CreateBook() {
 
+    const postBook = (event) => {
+        event.preventDefault();
+        // Collect form input
+        const data = new FormData(event.target);
+
+        // Post Data to API
+        apiClient.post("/books",data, {
+            headers: {
+              "Content-Type": "application/json"
+            }
+        })
+        .then((response) =>{
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
+    }
 
     return (
         <>
-        <Navbar/>
-        <div className="min-h-screen bg-gray-100 text-gray-700 flex flex-row gap-8 ">
-                        <Sidebar />
+            <Navbar />
+            <div className="min-h-screen bg-gray-100 text-gray-700 flex flex-row gap-8 ">
+                <Sidebar />
 
-        <div className="flex flex-row w-[90%] gap-x-20 m-auto pt-10">
-            <div className="w-[50%]">
-                <form className="bg-white p-8 rounded-lg shadow-md border border-gray-300  mx-auto py-6 px-10 flex flex-col  ">
-                    <div className="text-3xl font-bold m-auto"><h1>Fill Up Book Details</h1></div>
-                    <div className="flex flex-col p-4">
-                        <label htmlFor="type">Book Name</label>
-                        <input type="text" name="bookname" id="bookname" placeholder="Enter Book Name"  className="  border p-2 rounded-md"/>
-                    </div>
-                    <div className="flex flex-col p-4">
-                        <label htmlFor="type">Author's Name</label>
-                        <input type="text" name="author name" placeholder="Enter Author's Name" className=" p-2 rounded-md border" />
-                    </div>
-                    <div className="flex flex-row px-4 py-2 justify-between">
-                        <div className="flex flex-col">
-                            <label htmlFor="type">ISB Number</label>
-                            <input type="number" name="Booknumber" placeholder="Enter ISB Number" className=" p-2 rounded-md border"  />
-                        </div>
+                <div className="flex flex-row w-[90%] gap-x-20 m-auto pt-10">
+                    <div className="w-[50%]">
+                        <form onSubmit={postBook} className="bg-white p-8 rounded-lg shadow-md border border-gray-300  mx-auto py-6 px-10 flex flex-col  ">
+                            <div className="text-3xl font-bold m-auto"><h1>Fill Up Book Details</h1></div>
+                            <div className="flex flex-col p-4">
+                                <label htmlFor="type">Book Title</label>
+                                <input type="text" name="title" id="bookname" placeholder="Enter Book Title" className="  border p-2 rounded-md" />
+                            </div>
+                            <div className="flex flex-col p-4">
+                                <label htmlFor="type">Author's Name</label>
+                                <input type="text" name="author" placeholder="Enter Author's Name" className=" p-2 rounded-md border" />
+                            </div>
 
-                        <div className="flex flex-col p-4">
-                            <select name="Genre" id="" placeholder="Enter Book Name" className=" p-2 rounded-md border" >
-                                <option selected disabled >Choose Genre</option>
-                                <option value="drama">Horror</option>
-                                <option value="horror">Horror</option>
-                                <option value="fiction">Fiction</option>
-                                <option value="romance">Romance</option>
-                                <option value="thriller">Thriller</option>
-                                <option value="non-fiction">Non-fiction</option>
-                            </select>
-                        </div>
-                    </div>
+                            <div className="flex flex-row px-4 py-2 justify-between">
+                                <div className="flex flex-col">
+                                    <label htmlFor="type">Publication Year</label>
+                                    <input type="number" name="publicationYear" placeholder="Enter Publication Year" className=" py-2 rounded-md border" /></div>
 
-                    <div className="flex flex-col p-4">
-                        <label htmlFor="type">Book Name</label>
-                        <textarea name="bookdescription" placeholder="Enter Book Description"  className=" p-2  pb-40 rounded-md border " />
-                    </div>
+                                <div className="flex flex-col">
+                                    <label htmlFor="type">Publication Company</label>
+                                    <input type="text" name="publishingCompany" placeholder="Enter Publication Company" className=" p-2 rounded-md border" /></div>
+                            </div>
+
+                            <div className="flex flex-row px-4 py-2 justify-between">
+                                <div className="flex flex-col">
+                                    <label htmlFor="type">ISB Number</label>
+                                    <input type="number" name="isbn" placeholder="Enter ISB Number" className=" p-2 rounded-md border" />
+                                </div>
+
+                                <div className="flex flex-col p-4">
+                                    <select name="genre" id="" className=" p-2 rounded-md border" >
+                                        <option selected disabled >Choose Genre</option>
+                                        <option value="history">History</option>
+                                        <option value="science">Science</option>
+                                        <option value="fiction">Fiction</option>
+                                        <option value="romance">Romance</option>
+                                        <option value="fantasy">Fantasy</option>
+                                        <option value="programmming">Programming</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div className="flex flex-col p-4">
+                                <label htmlFor="type">Synopsis</label>
+                                <textarea name="synopsis" placeholder="Enter Book Synopsis" className=" p-2  pb-40 rounded-md border " />
+                            </div>
 
 
-                    <div>
-                        <button type="submit" className="  bg-amber-600 py-2 px-6 font-bold p-4 rounded-md">Add Book </button>
+                            <div className="flex flex-row gap-10">
+                                <button type="submit" className=" bg-amber-600 py-2 px-6 font-bold p-4 rounded-md">Add Book </button>
+                                <button type="submit" className="  bg-amber-600 py-2 px-6 font-bold p-4 rounded-md">Cancel</button>
+                            </div>
+                        </form>
                     </div>
-                </form>
+                    <div className="flex flex-col p-4  ">
+                        <h1 className="text-4xl font-bold pb-15">Your <span className="text-amber-500"> Input </span> <br />Helps Others To Read</h1>
+                        <p className="text-3xl ">Most purchased books.</p>
+                    </div>
+                </div>
             </div>
-            <div className="flex flex-col p-4  ">
-             <h1 className="text-4xl font-bold pb-15">Your <span className="text-amber-500"> Input </span> <br />Helps Others To Read</h1>
-             <p className="text-3xl ">Most purchased books.</p>
-            </div>
-        </div>
-        </div>
 
-    </>
+        </>
 
     );
 }
--- src/pages/EditBook.jsx
+++ src/pages/EditBook.jsx
@@ -16,25 +16,36 @@ export default function EditBook() {
                         <div className="flex flex-row justify-evenly py-3">
                             <div className="flex flex-col"> 
                                 <label htmlFor="type">Book Title</label>
-                                <input type="text" name="BookTitle" placeholder="Enter Book Title" className=" py-2 rounded-md border" /></div>
+                                <input type="text" name="title" placeholder="Enter Book Title" className=" py-2 rounded-md border" /></div>
 
                             <div className="flex flex-col">
                                 <label htmlFor="type">Author's Name</label>
-                                <input type="text" name="AuthorName" placeholder="Enter Author's Name" className=" p-2 rounded-md border" /></div>
+                                <input type="text" name="author" placeholder="Enter Author's Name" className=" p-2 rounded-md border" /></div>
                         </div>
+
+                        <div className="flex flex-row justify-evenly py-3">
+                            <div className="flex flex-col"> 
+                                <label htmlFor="type">Publication Year</label>
+                                <input type="date" name="publicationYear" placeholder="Enter Publication Year" className=" py-2 rounded-md border" /></div>
+
+                            <div className="flex flex-col">
+                                <label htmlFor="type">Publication Company</label>
+                                <input type="text" name="publicationCompany" placeholder="Enter Publication Company" className=" p-2 rounded-md border" /></div>
+                        </div>
+
                         <div className="flex flex-row gap-10 px-12">
                             <div className="flex flex-col">
                                 <label htmlFor="type">ISB Number</label>
-                                <input type="number" name="ISB Number" placeholder="Enter Book Number" className=" p-2 rounded-md border" /></div>
+                                <input type="number" name="isbn" placeholder="Enter Book Number" className=" p-2 rounded-md border" /></div>
 
-                            <select name="Genre" id="" placeholder="Enter Book Name" className="px-6 rounded-md border h-10" >
+                            <select name="genre" id="" placeholder="Enter Book Name" className="px-6 rounded-md border h-10" >
                                 <option selected disabled>Choose Genre</option>
-                                <option value="drama">Horror</option>
+                                <option value="history">History</option>
                                 <option value="horror">Horror</option>
                                 <option value="fiction">Fiction</option>
                                 <option value="romance">Romance</option>
-                                <option value="thriller">Thriller</option>
-                                <option value="non-fiction">Non-fiction</option>
+                                <option value="fantasy">Fantasy</option>
+                                <option value="programming">Programming</option>
                             </select>
                         </div>
 
@@ -43,36 +54,28 @@ export default function EditBook() {
                     <form className="bg-[rgba(255,255,255,0.4)] w-[100%] h-[40%] rounded-lg shadow-md border border-gray-300  mx-auto py-6 px-10 ">
                         <p className="text-xl text-center font-bold ">What to Edit</p>
                         <div className="flex flex-row gap-4 justify-evenly">
-                            <div className="flex flex-col gap-2">
-                                <label htmlFor="file">Check Availability</label>
-                                <select name="Availability" id="" placeholder="Check Availability" className="rounded-md border h-10" >
-                                    <option selected disabled>Availability</option>
-                                    <option value="Availability">Available</option>
-                                    <option value="Availability">Not Available</option>
-                                </select>
-                            </div>
                             <div className="flex flex-col gap-2 px-12 pb-2">
-                                <label htmlFor="file">Book Description</label>
-                                <input type="text" name="BookDescription" placeholder="Enter Book Description" className=" p-2 rounded-md border" />
+                                <label htmlFor="file">Synopsis</label>
+                                <input type="text" name="synopsis" placeholder="Enter Book Synopsis" className=" p-2 rounded-md border" />
                             </div>
-                        </div>
                         <div className="flex flex-row gap-4">
 
-                            <select name="Genre" id="" placeholder="Enter Book Name" className=" p-2 rounded-md border" >
+                            <select name="genre" id="" placeholder="Enter Book Name" className=" p-2 rounded-md border" >
                                 <option selected disabled>Choose Genre</option>
-                                <option value="drama">Horror</option>
-                                <option value="horror">Horror</option>
+                                <option value="history">History</option>
+                                <option value="science">Science</option>
                                 <option value="fiction">Fiction</option>
                                 <option value="romance">Romance</option>
-                                <option value="thriller">Thriller</option>
-                                <option value="non-fiction">Non-fiction</option>
+                                <option value="fantasy">Fantasy</option>
+                                <option value="programming">Programming</option>
                             </select>
+                        </div>
                         </div>
 
                     </form>
-                    <div className="flex flex-row gap-5">
-                        <button type="submit">Save Changes</button>
-                        <button type="submit">Cancel</button>
+                    <div className="flex flex-row gap-10">
+                        <button type="submit" className="  bg-amber-600 py-2 px-6 font-bold p-4 rounded-md mt-4">Save Changes</button>
+                        <button type="submit" className="  bg-amber-600 py-2 px-6 font-bold p-4 rounded-md mt-4">Cancel</button>
                     </div>
                 </div>
             </div>
