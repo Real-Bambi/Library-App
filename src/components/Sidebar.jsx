32599597308b6f5cb717d1a40989999139e90d76
Genevieve <genevieveappia2@gmail.com>
2025-06-12 13:22:13 +0000

    logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ export default function Sidebar() {
 return (
     
  
-  <aside className="w-64 h-screen bg-[#FBDB93] text-black flex flex-col justify-between py-8 px-6 shadow-xl shadow-blue-300">
+  <aside className="w-70 h-screen bg-[#FBDB93] text-black flex flex-col justify-between py-8 px-6 shadow-xl shadow-blue-300">
     <div>
       <div className="mb-10 text-left">
         <h1 className="text-xl leading-none italic tracking-wide">
@@ -26,18 +26,15 @@ return (
            <LibraryBig /> My Shelf
         </Link>
         <Link to="/create-book" className="flex items-center gap-3 hover:text-[#BE5B50]">
-           <HandHeart /> Contribute
+           <HandHeart /> Add Book
         </Link>
       </nav>
     </div>
-
-    
-    <div class="space-y-2 text-sm text-gray-600 py-30">
+    <div className="space-y-2 text-sm text-gray-600 py-30">
       <Link to="#" className="text-[#641B2E] block">About</Link>
       <Link to="#" className="text-[#641B2E] block">Support</Link>
       <Link to="#" className="text-[#641B2E] block">Terms & Condition</Link>
     </div>
-
   </aside>
 );
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,17 @@
 import NotFoundImg from "../assets/images/404.jpg"
-import { ChevronsDown } from 'lucide-react';
+import {  ArrowBigDownDash } from 'lucide-react';
 import { Link } from "react-router";
 
 export default function NotFound() {
     return (
         <>
-            <div className="bg-[url(./assets/images/404.jpg)] h-[100vh] animate-pulse bg-cover w-[50%] ml-50 mb-20 items-center">
+            <div className="bg-[url(./assets/images/404.jpg)] h-[95vh] animate-pulse bg-cover w-[50%] ml-50 items-center">
             </div>
 
             
 
             <div>
-                <Link to="/"><button className="rounded-[10px] font-extrabold animate-none bg-black text-[2opx] text-[#FCF259] items-center p-4 hover: cursor-pointer  ml-70">HOMEPAGE</button></Link>
+                <Link to="/"><button className="rounded-[10px] font-extrabold animate-none bg-black text-[2opx] text-[#FCF259] items-center p-4 hover:bg-[#BE5B50] cursor-pointer ml-120">GO HOME ↤</button></Link>
 
             </div>
         </>
