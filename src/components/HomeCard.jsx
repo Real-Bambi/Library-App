26e19101e52cd9541b87f59cf273068db4220a12
Sally <salomeytackie555@gmail.com>
2025-06-12 16:09:45 +0000

    updating changes in homepage

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -4,9 +4,9 @@
 export default function HomeCard ({image,title}) {
     
     return(
-        <div className="bg-white shadow-md rounded p-2"> 
+        <div className="bg-white shadow-md rounded p-2 transform hover:scale-[1.10] transition "> 
             <img src= {image} alt="drama image"  />
-                  <h2 className="text-lg font-bold text-center">{title}</h2>
+                  <h2 className=" text-lg font-bold text-center text-orange-950">{title}</h2>
 
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,34 +15,34 @@ export default function Home() {
 
 
 const cards = [
-  { id: 1, image: FictionImage, title: "Fiction" },
-  { id: 2, image: NonFictionImage, title: "Non-Fiction" },
-  { id: 3, image: HorrorImage, title: "Horror" },
-  { id: 4, image: ThrillerImage, title: "Thriller" },
-  { id: 5, image: SciFiImage, title: "Sci-Fic" },
-  { id: 6, image: DramaImage, title: "Drama" },
+  { id: 1, image: FictionImage, title: "FICTION" },
+  { id: 2, image: NonFictionImage, title: "HISTORY" },
+  { id: 3, image: HorrorImage, title: "FANTASY" },
+  { id: 4, image: ThrillerImage, title: "PROGRAMMING" },
+  { id: 5, image: SciFiImage, title: "SCIENCE" },
+  { id: 6, image: DramaImage, title: "ROMANCE" },
 ];
 
     return (
 
-      <div className='relative h-full'>
+      <div className=' h-full'>
         {/* <div className="absolute inset-1 bg-gradient-to-r from-black-700 via-black/50 to-black/90 z-2"></div> */}
         
-          <div className="  bg-[url('./assets/images/booklib.jpg'))] bg-cover ">
+          <div className="  bg-[url('./assets/images/booklib.jpg'))] bg-cover h-[100vh] md:h-full ">
           
 
             <div >
-                <h1 className="font-bold text-center text-6xl pt-10"><span className="text-black">Welcome to</span> <span className="text-orange-700 "> BOOK SHELF</span>.</h1>
+                <h1 className="font-bold text-center text-6xl pt-10"><span className="text-black">Welcome to</span> <span className="text-orange-500 "> BOOK SHELF</span>.</h1>
                 <h2 className='font-medium text-center text-5xl pt-10'>Discover the best books <span className="text-white">HERE!</span></h2>
                 {/* <ArrowBigDown className=' flex flex-col size-30 items-center text-red-400 animate-bounce'/> */}
             </div>
             <div className="flex  justify-center pt-6">
-              <Link to={'/book-page'}><button className="bg-orange-800 hover:bg-orange-500 px-6 py-2 rounded text-white">VIEW HERE</button></Link>
+              <Link to={'/book-page'}><button className="bg-orange-500 hover:bg-orange-800 px-6 py-2 rounded-2xl text-white">VIEW HERE</button></Link>
             </div>
-            <div className='w-[80%] grid grid-cols-3 gap-4  pt-8 mx-auto '>
+            <div className='w-[80%] grid grid-cols-3 gap-4   pt-8 mx-auto '>
                 {cards.map(item  => {
                     return (
-                        <Link to={'#'}><HomeCard key={item.id} image={item.image} title={item.title} /></Link>
+                        <Link to={'/book-page'}><HomeCard key={item.id} image={item.image} title={item.title} /></Link>
                     )
                 })}
             </div>
