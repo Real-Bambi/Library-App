326bdf9a5120cae29cd20882bc172c9f23028f7a
Genevieve <genevieveappia2@gmail.com>
2025-06-12 16:32:40 +0000

    resp

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,20 +1,25 @@
-import NotFoundImg from "../assets/images/404.jpg"
-import {  ArrowBigDownDash } from 'lucide-react';
-import { Link } from "react-router";
+import NotFoundImg from "../assets/images/404.jpg";
+import { ArrowBigDownDash } from "lucide-react";
+import { Link } from "react-router"; 
 
 export default function NotFound() {
-    return (
-        <>
-            <div className="bg-[url(./assets/images/404.jpg)] h-[95vh] animate-pulse bg-cover w-[50%] ml-50 items-center">
-            </div>
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen px-4 bg-white">
+      
+      <div
+        className="w-full max-w-md h-96 bg-cover bg-center animate-pulse rounded-lg shadow-lg relative"
+        style={{ backgroundImage: `url(${NotFoundImg})` }}
+      >
+       
+        <ArrowBigDownDash className="absolute bottom-[-20px] left-1/2 -translate-x-1/2 text-black animate-bounce" size={40} />
+      </div>
 
-            
-
-            <div>
-                <Link to="/"><button className="rounded-[10px] font-extrabold animate-none bg-black text-[2opx] text-[#FCF259] items-center p-4 hover:bg-[#BE5B50] cursor-pointer ml-120">GO HOME ↤</button></Link>
-
-            </div>
-        </>
-    );
+      
+      <Link to="/">
+        <button className="mt-10 rounded-xl font-extrabold bg-black text-yellow-300 text-lg px-6 py-3 hover:bg-[#BE5B50] transition">
+          GO HOME ↤
+        </button>
+      </Link>
+    </div>
+  );
 }
-
