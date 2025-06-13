08cc783e1f3c20485b5ce0e9ccd5cadbb2909f36
Dede <dedenyengor09@gmail.com>
2025-06-13 13:46:06 +0000

    update

--- src/pages/EditBook.jsx
+++ src/pages/EditBook.jsx
@@ -50,7 +50,7 @@ export default function EditBook() {
 
         <>
             <Navbar />
-            <div className="min-h-screen bg-gray-100 text-gray-700 flex flex-row gap-8 ">
+            <div className="h-[140vh] bg-gray-100 text-gray-700 flex flex-row gap-8 ">
                 <Sidebar />
                 </div>
 
