13d083a473b83e49e6696dafe28cac27a5ca8592
Sally <salomeytackie555@gmail.com>
2025-06-13 10:28:35 +0000

    final update to the homepage

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -4,9 +4,9 @@
 export default function HomeCard ({image,title}) {
     
     return(
-        <div className="bg-white hover:bg-[#FBDB93] shadow-md rounded p-2 transform hover:scale-[1.10] transition "> 
-            <img src= {image} alt="drama image"  />
-                  <h2 className=" text-lg font-bold text-center text-orange-950">{title}</h2>
+        <div className="bg-white hover:bg-[#FBDB93] shadow-md rounded p-2 transform hover:scale-[1.10] transition  md:w-[100%] "> 
+            <img src= {image} alt="drama image"   />
+                  <h2 className=" text-sm md:text-lg font-bold text-center text-orange-950">{title}</h2>
 
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,14 +32,14 @@ const cards = [
           
 
             <div >
-                <h1 className="font-bold text-center text-6xl pt-10"><span className="text-white">Welcome to</span> <span className="text-[#BE5B50] "> BOOK SHELF</span>.</h1>
-                <h2 className='font-medium text-center text-5xl pt-10 text-white'>Discover the best books <span className="text-[#FBDB93]">HERE!</span></h2>
+                <h1 className="font-bold text-center text-2xl md:text-6xl pt-10"><span className="text-white">Welcome to</span> <span className="text-[#BE5B50] "> BOOK SHELF</span>.</h1>
+                <h2 className='font-medium text-center text-3xl md:text-5xl pt-10 text-white'>Discover the best books <span className="text-[#FBDB93]">HERE!</span></h2>
                 {/* <ArrowBigDown className=' flex flex-col size-30 items-center text-red-400 animate-bounce'/> */}
             </div>
             <div className="flex  justify-center pt-6">
               <Link to={'/book-page'}><button className="bg-[#8A2D3B] hover:bg-[#641B2E] px-6 py-2 rounded-lg text-white">View Here →</button></Link>
             </div>
-            <div className='w-[80%] grid grid-cols-3 gap-4   pt-8 mx-auto '>
+            <div className='w-[80%] grid grid-cols-2 md:grid-cols-3 gap-4   pt-8 mx-auto '>
                 {cards.map(item  => {
                     return (
                         <Link to={'/book-page'}><HomeCard key={item.id} image={item.image} title={item.title} /></Link>
